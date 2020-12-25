21ae3be66be3c30ca58a296dcec51faf6cf01735
Michael Penn <mwpenn94@gmail.com>
2020-12-25 05:17:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+# put argument(s) below
+def my_each(array)
+# code here
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield(array[i])
+      i = i +1
+    end
+
+    array
+  else
+    return nil
+  end
 end
